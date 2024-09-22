cd373d047f8bfe6f1a95c3adc109aff96021f1aa
Aosaf098 <aosafibad10@gmail.com>
2024-09-22 18:54:18 +0600

    Transaction History

--- home.js
+++ home.js
@@ -1,34 +1,63 @@
+// Add Money Button
+
 document.getElementById('add-money-btn').addEventListener('click', function() {
     document.getElementById('add-money-input').classList.remove('hidden')
     document.getElementById('cash-out-input').classList.add('hidden')
+    document.getElementById('transaction-history-box').classList.add('hidden')
+    document.getElementById('logo-img').classList.remove('-mt-80')
+    document.getElementById('logo-img').classList.remove('lg:-mt-96')
 })
+
+// Cash Out Button
+
+document.getElementById('cash-out-btn').addEventListener('click', function() {
+    document.getElementById('cash-out-input').classList.remove('hidden')
+    document.getElementById('add-money-input').classList.add('hidden')
+    document.getElementById('transaction-history-box').classList.add('hidden')
+    document.getElementById('logo-img').classList.remove('-mt-80')
+    document.getElementById('logo-img').classList.remove('lg:-mt-96')
+})
+
+// Transaction History Button
+
+document.getElementById('transaction-history-btn').addEventListener('click', function() {
+    document.getElementById('transaction-history-box').classList.remove('hidden')
+    document.getElementById('add-money-input').classList.add('hidden')
+    document.getElementById('cash-out-input').classList.add('hidden')
+    document.getElementById('logo-img').classList.add('-mt-80')
+    document.getElementById('logo-img').classList.remove('lg:-mt-96')
+
+
+})
+
+// Add Money Feature
 
 document.getElementById('deposit-btn').addEventListener('click', function(e) {
     e.preventDefault()
-    // document.getElementById('deposit-btn').style.backgroundColor = 'red'
     
     const addAmount = Number(document.getElementById('add-amount-input').value)
     const addPin = document.getElementById('add-pin-input').value
-
+    
     const currentBalance = Number(document.getElementById('current-balance').innerText)
-
+    
     if(addPin === '1234rd') {
         const updatedBalance = addAmount + currentBalance
         document.getElementById('current-balance').innerText = updatedBalance
+        const history = document.getElementById('transaction-history-content')
+        const newHistory = document.createElement('h5')
+        newHistory.style.fontSize = '18px'
+        newHistory.innerText = `Added: ${addAmount} Total Balance: ${updatedBalance}`
+        history.appendChild(newHistory)
     } else {
         alert('Invalid Pin')
     }
     document.getElementById('add-amount-input').value = ''
     document.getElementById('add-pin-input').value = ''
-
+    
 })
 
+// Cash Out Feature
 
-document.getElementById('cash-out-btn').addEventListener('click', function() {
-    document.getElementById('cash-out-input').classList.remove('hidden')
-    document.getElementById('add-money-input').classList.add('hidden')
-
-})
 
 document.getElementById('withdraw-btn').addEventListener('click', function(e) {
     e.preventDefault()
@@ -45,6 +74,11 @@ document.getElementById('withdraw-btn').addEventListener('click', function(e) {
         } else{
             const updatedBalance = Math.abs(currentBalance - subAmount)
             document.getElementById('current-balance').innerText = updatedBalance
+            const history = document.getElementById('transaction-history-content')
+            const newHistory = document.createElement('h5')
+            newHistory.style.fontSize = '18px'
+            newHistory.innerText = `Reduced: ${subAmount} Total Balance: ${updatedBalance}`
+            history.appendChild(newHistory)
         }
     } else {
         alert('Invalid Pin')
